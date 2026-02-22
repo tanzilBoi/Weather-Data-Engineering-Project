5a5f393c1d45777a752513b16e1c389df0f2d8e5
Tanzil Ul Ameen <120655096+tanzilBoi@users.noreply.github.com>
2026-02-22 15:26:13 +0530

    Update weather_api.py

--- scripts/weather_api.py
+++ scripts/weather_api.py
@@ -1,1 +1,21 @@
+# Ingesting Bangalore city weather from weather API into Raw file 
+import requests
+import json
+import os
 
+API_KEY = "your_api_key_here" # You can paste your weather API here, I have personally used https://openweathermap.org/ to generate the API
+
+CITY = "Bangalore"
+
+URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+
+response = requests.get(URL)
+
+data = response.json()
+
+os.makedirs("../data/raw", exist_ok=True)
+
+with open("../data/raw/weather_data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+print("Weather data successfully ingested!")
