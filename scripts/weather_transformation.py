eafa22fcce962344d5ed9ebd84fbd8f19c820b9f
Tanzil Ul Ameen <120655096+tanzilBoi@users.noreply.github.com>
2026-02-22 15:29:03 +0530

    Update weather_transformation.py

--- scripts/weather_transformation.py
+++ scripts/weather_transformation.py
@@ -1,1 +1,27 @@
+# Transforming raw JSON data to CSV using Pyspark
+import json
+import pandas as pd
+import os
 
+with open("../data/raw/weather_data.json") as file:
+    data = json.load(file)
+
+city = data["name"]
+temperature = data["main"]["temp"]
+humidity = data["main"]["humidity"]
+weather = data["weather"][0]["main"]
+country = data["sys"]["country"]
+
+df = pd.DataFrame({
+    "City": [city],
+    "Temperature": [temperature],
+    "Humidity": [humidity],
+    "Weather": [weather],
+    "Country": [country]
+})
+
+os.makedirs("../data/processed", exist_ok=True)
+
+df.to_csv("../data/processed/weather_processed.csv", index=False)
+
+print("Data successfully transformed!")
